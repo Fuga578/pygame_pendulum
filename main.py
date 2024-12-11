4a4641b2f54c85927df98c8183511b24b8dfa228
DESKTOP-579G82A\fuga <kochikaze@softbank.ne.jp>
2024-12-11 21:36:23 +0900

    糸が切れた場合の処理実装

--- main.py
+++ main.py
@@ -30,7 +30,13 @@ offset_deg = 90
 angle = math.radians(angle_deg + offset_deg)     # 角度 [rad]
 angle_velocity = 0  # 角速度
 angle_acceleration = 0  # 各加速度
-g = 0.5     # 重力
+gravity = 0.5     # 重力
+is_cut = False
+player_velocity = pygame.math.Vector2()
+player_point = pygame.math.Vector2(
+    fulcrum_point.x + length * math.sin(angle),
+    fulcrum_point.y + length * math.cos(angle)
+)
 
 while True:
 
@@ -38,16 +44,21 @@ while True:
     screen.fill(WHITE)
 
     # 振り子の演算
-    angle_acceleration = -(g / length) * math.sin(angle)  # 角加速度の計算
-    angle_velocity += angle_acceleration  # 角速度の更新
-    angle_velocity *= 0.99  # 減衰効果
-    angle += angle_velocity  # 角度の更新
+    if not is_cut:
+        angle_acceleration = -(gravity / length) * math.sin(angle)  # 角加速度の計算
+        angle_velocity += angle_acceleration  # 角速度の更新
+        angle_velocity *= 0.99  # 減衰効果
+        angle += angle_velocity  # 角度の更新
 
-    # プレイヤーの位置の計算
-    player_point = pygame.math.Vector2(
-        fulcrum_point.x + length * math.sin(angle),
-        fulcrum_point.y + length * math.cos(angle)
-    )
+        # プレイヤーの位置の計算
+        player_point = pygame.math.Vector2(
+            fulcrum_point.x + length * math.sin(angle),
+            fulcrum_point.y + length * math.cos(angle)
+        )
+
+    else:
+        player_point += player_velocity
+        player_velocity.y += gravity
 
     # 支点の描画
     pygame.draw.circle(screen, GREEN, (fulcrum_point.x, fulcrum_point.y), 10)
@@ -56,17 +67,28 @@ while True:
     pygame.draw.circle(screen, RED, (player_point.x, player_point.y), 30)
 
     # 糸の描画
-    pygame.draw.line(screen, BLACK, (fulcrum_point.x, fulcrum_point.y), (player_point.x, player_point.y), 3)
+    if not is_cut:
+        pygame.draw.line(screen, BLACK, (fulcrum_point.x, fulcrum_point.y), (player_point.x, player_point.y), 3)
 
     # イベントの取得
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        # キーボード押下
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+        # マウスクリック
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # 左クリック
+            if event.button == 1:
+                is_cut = True
+                player_velocity = pygame.math.Vector2(
+                    angle_velocity * length * math.cos(angle),
+                    -angle_velocity * length * math.sin(angle)
+                )
 
     # 更新
     pygame.display.update()
