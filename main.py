1dc11abed65abee3d3b102847f5f83b153646dcd
DESKTOP-579G82A\fuga <kochikaze@softbank.ne.jp>
2024-12-12 20:56:37 +0900

    クリック位置を支点に変更

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ INPUTS = {
     "right": False
 }
 
-# 設定
-fulcrum_point = pygame.math.Vector2(screen_width // 2, screen_height // 2)  # 支点位置
+# 初期設定
+fulcrum_point = pygame.math.Vector2()  # 支点位置
 length = 100    # 糸の長さ
 angle_deg = 45  # 角度 [deg]
 offset_deg = 90
@@ -39,23 +39,34 @@ angle = math.radians(angle_deg + offset_deg)     # 角度 [rad]
 angle_velocity = 0  # 角速度
 angle_acceleration = 0  # 各加速度
 gravity = 0.5     # 重力
-is_cut = False
+is_hook = False
 player_velocity = pygame.math.Vector2()
-player_point = pygame.math.Vector2(
-    fulcrum_point.x + length * math.sin(angle),
-    fulcrum_point.y + length * math.cos(angle)
-)
-swing_force = 0.002
+player_point = pygame.math.Vector2(screen_width // 2, screen_height // 2)
+# swing_force = 0.002
+"""
+length：swing_force = 100:0.002
+-> swing_force = 0.2 / length
+"""
 
 while True:
 
     # 背景の塗りつぶし
     screen.fill(WHITE)
 
+    # プレイヤーの描画
+    pygame.draw.circle(screen, RED, (player_point.x, player_point.y), 30)
+
     # 振り子の演算
-    if not is_cut:
+    if is_hook:
+
+        # 支点の描画
+        pygame.draw.circle(screen, GREEN, (fulcrum_point.x, fulcrum_point.y), 10)
+
+        # 糸の描画
+        pygame.draw.line(screen, BLACK, (fulcrum_point.x, fulcrum_point.y), (player_point.x, player_point.y), 3)
 
         # 入力による加速
+        swing_force = 0.2 / length
         if INPUTS["left"]:
             angle_velocity -= swing_force
         if INPUTS["right"]:
@@ -75,16 +86,6 @@ while True:
     else:
         player_point += player_velocity
         player_velocity.y += gravity
-
-    # 支点の描画
-    pygame.draw.circle(screen, GREEN, (fulcrum_point.x, fulcrum_point.y), 10)
-
-    # プレイヤーの描画
-    pygame.draw.circle(screen, RED, (player_point.x, player_point.y), 30)
-
-    # 糸の描画
-    if not is_cut:
-        pygame.draw.line(screen, BLACK, (fulcrum_point.x, fulcrum_point.y), (player_point.x, player_point.y), 3)
 
     # イベントの取得
     for event in pygame.event.get():
@@ -118,7 +119,19 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             # 左クリック
             if event.button == 1:
-                is_cut = True
+                # 更新
+                is_hook = True
+                mx, my = pygame.mouse.get_pos()
+                fulcrum_point = pygame.math.Vector2(mx, my)     # 支点位置
+                length = math.sqrt((mx - player_point.x)**2 + (my - player_point.y)**2)  # 糸の長さ
+                angle = math.atan2(player_point.x - mx, player_point.y - my)
+                angle_velocity = 0  # 角速度
+                angle_acceleration = 0  # 各加速度
+
+        # マウス解放
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                is_hook = False
                 player_velocity = pygame.math.Vector2(
                     angle_velocity * length * math.cos(angle),
                     -angle_velocity * length * math.sin(angle)
